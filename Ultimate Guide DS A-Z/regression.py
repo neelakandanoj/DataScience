31303ca29408e980ee14aa00102bbe1624b52b71
neelakandan <neelakandan2001kpm@gmail.com>
2024-06-14 23:52:53 +0530

    'dd'

--- Ultimate Guide DS A-Z/regression.py
+++ Ultimate Guide DS A-Z/regression.py
@@ -12,6 +12,8 @@ y = dataset.iloc[:, -1].values
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
+
+
 # Fitting Gradient Boosting to the Training set
 from sklearn.ensemble import GradientBoostingRegressor
 regressor = GradientBoostingRegressor()
